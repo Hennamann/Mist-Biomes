79c7f10d4be7617aa6beaf8a40901fc9ab04a350
Ole Henrik Stabell <ole@henrikstabell.com>
2018-02-05 09:04:46 +0100

    sync

--- src/main/java/com/henrikstabell/mistbiomes/content/biomes/BiomeMistForest.java
+++ src/main/java/com/henrikstabell/mistbiomes/content/biomes/BiomeMistForest.java
@@ -17,6 +17,14 @@ public class BiomeMistForest extends Biome implements IBiomeMist {
         this.spawnableMonsterList.add(new Biome.SpawnListEntry(EntityZombie.class, 20, 8, 16));
     }
 
+    public int getModdedBiomeGrassColor(int original) {
+        return 1604134;
+    }
+
+    public int getModdedBiomeFoliageColor(int original) {
+        return 877336;
+    }
+
     @Override
     public float getMistDensity(int var1, int var2, int var3) {
         return MistBiomesConfig.mistDensity;
